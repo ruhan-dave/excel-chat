216317f9a8d03b4f86d8ec7c6dccb7a5d4f33936
Shaunak Badani <shaunak.badani@duke.edu>
2025-02-11 23:08:14 -0500

    Added very simple tutorial

---
+++ README.md
@@ -0,0 +1,37 @@
+# RagSheets
+
+> A repository to handle queries for excel sheets uploaded by the user to an agent.
+
+### How to run
+
+- Ensure that the following keys are set on your environment:
+```
+OPENAI_API_KEY
+SUPABASE_KEY
+SUPABASE_URL
+```
+
+- Ensure that you have created necessary tables in Supabase (Vector DB) using table and function definitions found [here](https://www.aispaceship.io/docs/rag-beginner-guide/create-embeddings).
+
+- Create a virtual environment, and install dependencies:
+```
+python -m venv .venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+- To run the notebook, ensure that you install a kernel:
+
+    ```
+    python -m ipykernel install --user --name RAG
+    ```
+    -  Start jupyter notebook:
+    ```
+    jupyter-notebook
+    ```
+    - And select the `RAG` kernel during runtime.
+
+- The script is a copy of the notebook. To run the rag script, 
+```
+python rag.py
+```
---
+++ rag.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+import openai
+import os
+import supabase
+from time import sleep
+from supabase import create_client, Client
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+client = OpenAI()
+
+# Initialize supabase client
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase_client: Client = create_client(url, key)
+
+def insert_embedding(doc_name: str, embedding: list[float]):
+    supabase_client.table("docs").upsert({"doc_name": doc_name, "embedding": embedding}).execute()
+    
+def get_embedding(doc_name: str) -> list[float]:
+    response = supabase_client.table("docs").select("embedding").eq("doc_name", doc_name).execute()
+    return response.data[0]["embedding"]
+
+def create_embedding_openai(text: str):
+    response = client.embeddings.create(input=text, model="text-embedding-3-small", encoding_format="float")
+    return response.data[0].embedding
+
+def create_embedding_from_files(files: list[str]):
+    for file in files:
+        with open(file, "r") as f:
+            text = f.read()
+            embedding = create_embedding_openai(text)
+            insert_embedding(file, embedding)
+
+def query_embedding(query: str):
+    embedding = create_embedding_openai(query)
+    response = supabase_client.rpc("query_documents", {"query_embedding": embedding}).execute()
+    return response
+
+
+def run_rag():
+    # create_embedding_from_files(["docs/doc1.txt", "docs/doc2.txt"])
+    response = query_embedding("dog")
+    print(response)
+
+
+if __name__ == "__main__":
+    run_rag()

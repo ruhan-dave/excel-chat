f9a858036032b65aa906c9b651feb90e4ea42f38
Shaunak Badani <shaunak.badani@duke.edu>
2025-02-14 13:31:29 -0500

    Added shadcn working with Geist

---
+++ frontend/vite-shadcn-boilerplate/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        The Joke Tax Chronicles
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Once upon a time, in a far-off land, there was a very lazy king who
+        spent all day lounging on his throne. One day, his advisors came to him
+        with a problem: the kingdom was running out of money.
+      </p>
+      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        The King's Plan
+      </h2>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <Button>Test me</Button>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App

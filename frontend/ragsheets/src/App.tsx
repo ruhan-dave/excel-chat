8e21eb1461442cb44b274289338f96bb11e1bbae
Shaunak Badani <shaunak.badani@duke.edu>
2025-02-14 14:06:14 -0500

    Folder renamed

--- frontend/ragsheets/src/App.tsx
+++ frontend/ragsheets/src/App.tsx
@@ -7,28 +7,36 @@ function App() {
 
   return (
     <>
+    <div>
+      <div className="header p-6 text-xl border-b">AppName</div>
+    <div className="min-h-screen p-8 pb-8 sm:p-8">      
+      <main className="max-w-4xl mx-auto flex flex-col gap-16">
       <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        The Joke Tax Chronicles
+        Super catchy tagline!
       </h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         Once upon a time, in a far-off land, there was a very lazy king who
         spent all day lounging on his throne. One day, his advisors came to him
-        with a problem: the kingdom was running out of money.
+        with a problem: the kingdom was running out of money (insert description)
       </p>
-      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        The King's Plan
-      </h2>
       </div>
-      <h1>Vite + React</h1>
+      <h1>AppName</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <Button>Test me</Button>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <Button className="w-xl">Test me</Button>
+
+      </main>
+
+    </div>
+    </div>
+
+      
     </>
   )
 }
